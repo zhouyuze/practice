fdc91fffb78df68c397b87ce588ea09c6b64e91d
Yuze Zhou <zhouyuze@hashdata.cn>
2017-09-18 10:09:29 +0800

    test mvn

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{"name": "inventory-connector","config": {"connector.class": "io.debezium.connector.postgresql.PostgresConnector","database.hostname": "postgres","database.port": "5432","database.user": "postgres","database.password": "postgres","database.dbname" : "postgres","database.server.name": "debezium1"}}'
+echo `pwd`

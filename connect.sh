575ed7b932cd87f8129551a2aeedb4a15057d2c1
Yuze Zhou <zhouyuze@hashdata.cn>
2017-09-22 15:03:16 +0800

    test

--- connect.sh
+++ connect.sh
@@ -7,4 +7,9 @@ docker run -it --name MySQL -p 127.0.0.1:3306:3306 \
 				-d mysql:latest \
 				--server-id=1 --log-bin=master --binlog_format=row
 
+until $(docker logs MySQL | grep -q "Server hostname (bind-address)")
+do
+    sleep 1
+done
+
 python connect.py
